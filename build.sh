3fc4fe57146abc2679415a22b44ac51d5441ad33
brymlee <brymlee1911@gmail.com>
2020-04-05 13:19:55 -0600

    Example compilation

--- build.sh
+++ build.sh
@@ -1,13 +1,2 @@
-echo $(git config user.email) > .user.email
-echo $(git config user.name) > .user.name
-cp /home/$(cat .whoami)/.ssh . -r
-cp /etc/passwd .
-mv passwd .passwd
-cp /etc/shadow .
-mv shadow .shadow
-cp /etc/group .
-mv group .group
-cp /etc/gshadow .
-mv gshadow .gshadow
 docker build -t java . --file Java.dockerfile
 docker build -t com.brymlee.common.composites . --no-cache --file Development.dockerfile
---
+++ sync.sh
@@ -0,0 +1,2 @@
+docker cp com.brymlee.common.composites:/com.brymlee.common.composites ..
+chown $(whoami) ../com.brymlee.common.composites -R
