6ca516ead87ab836248f2ce33743090d8560ae9f
brymlee <brymlee1911@gmail.com>
2020-04-11 11:43:06 -0600

    Adding appends for Tuple3

--- src/main/java/com.brymlee.common.composites/com/brymlee/common/composites/pub/Tuple3.java
+++ src/main/java/com.brymlee.common.composites/com/brymlee/common/composites/pub/Tuple3.java
@@ -1,5 +1,7 @@
 package com.brymlee.common.composites.pub;
 
+import static com.brymlee.common.composites.pub.TupleFactory.tuple;
+
 public class Tuple3<T, U, V>{
 	private final T t1;
 	private final U t2;
@@ -28,7 +30,50 @@ public class Tuple3<T, U, V>{
 	}
 
 	public Tuple2<U, V> tail(){
-		return TupleFactory.tuple(this.t2, this.t3);
+		return tuple(this.t2, this.t3);
 	}
 
+	public <W> Tuple4<T, U, V, W> append(final W w){
+		return tuple(this.t1, this.t2, this.t3, w);
+	}
+
+	public <W, X> Tuple5<T, U, V, W, X> append(final W w, final X x){
+		return tuple(this.t1, this.t2, this.t3, w, x);
+	}
+
+	public <W, X> Tuple5<T, U, V, W, X> append(final Tuple2<W, X> tuple){
+		return tuple(this.t1, this.t2, this.t3, tuple.t1(), tuple.t2());
+	}
+
+	public <W, X, Y> Tuple6<T, U, V, W, X, Y> append(final W w, final X x, final Y y){
+		return tuple(this.t1, this.t2, this.t3, w, x, y);
+	}
+
+	public <W, X, Y> Tuple6<T, U, V, W, X, Y> append(final Tuple3<W, X, Y> tuple){
+		return tuple(this.t1, this.t2, this.t3, tuple.t1(), tuple.t2(), tuple.t3());
+	}
+
+	public <W, X, Y, Z> Tuple7<T, U, V, W, X, Y, Z> append(final W w, final X x, final Y y, final Z z){
+		return tuple(this.t1, this.t2, this.t3, w, x, y, z);
+	}
+
+	public <W, X, Y, Z> Tuple7<T, U, V, W, X, Y, Z> append(final Tuple4<W, X, Y, Z> tuple){
+		return tuple(this.t1, this.t2, this.t3, tuple.t1(), tuple.t2(), tuple.t3(), tuple.t4());
+	}
+
+	public <W, X, Y, Z, A> Tuple8<T, U, V, W, X, Y, Z, A> append(final W w, final X x, final Y y, final Z z, final A a){
+		return tuple(this.t1, this.t2, this.t3, w, x, y, z, a);
+	}
+
+	public <W, X, Y, Z, A> Tuple8<T, U, V, W, X, Y, Z, A> append(final Tuple5<W, X, Y, Z, A> tuple){
+		return tuple(this.t1, this.t2, this.t3, tuple.t1(), tuple.t2(), tuple.t3(), tuple.t4(), tuple.t5());
+	}
+
+	public <W, X, Y, Z, A, B> Tuple9<T, U, V, W, X, Y, Z, A, B> append(final W w, final X x, final Y y, final Z z, final A a, final B b){
+		return tuple(this.t1, this.t2, this.t3, w, x, y, z, a, b);
+	}
+
+	public <W, X, Y, Z, A, B> Tuple9<T, U, V, W, X, Y, Z, A, B> append(final Tuple6<W, X, Y, Z, A, B> tuple){
+		return tuple(this.t1, this.t2, this.t3, tuple.t1(), tuple.t2(), tuple.t3(), tuple.t4(), tuple.t5(), tuple.t6());
+	}
 }
