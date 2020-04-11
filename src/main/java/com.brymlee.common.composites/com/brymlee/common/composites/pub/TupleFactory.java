6dac1daba0fbf1fb0970eac8ea281497a3b6cb5e
brymlee <brymlee1911@gmail.com>
2020-04-10 22:18:07 -0600

    Removing compiler warnings

--- src/main/java/com.brymlee.common.composites/com/brymlee/common/composites/pub/TupleFactory.java
+++ src/main/java/com.brymlee.common.composites/com/brymlee/common/composites/pub/TupleFactory.java
@@ -3,6 +3,6 @@ package com.brymlee.common.composites.pub;
 public class TupleFactory{
 
 	public static <T, U> Tuple2<T, U> tuple(final T t1, final U t2){
-		return new Tuple2(t1, t2);
+		return new Tuple2<T, U>(t1, t2);
 	}
 }
