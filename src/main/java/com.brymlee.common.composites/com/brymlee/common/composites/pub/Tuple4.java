36ee9363732f05e1d64602a99a67fc162c7c1147
brymlee <brymlee1911@gmail.com>
2020-04-10 23:13:55 -0600

    Adding Tuple4

---
+++ src/main/java/com.brymlee.common.composites/com/brymlee/common/composites/pub/Tuple4.java
@@ -0,0 +1,35 @@
+package com.brymlee.common.composites.pub;
+
+public class Tuple4<T, U, V, W>{
+	private final T t1;
+	private final U t2;
+	private final V t3;
+	private final W t4;
+
+	public Tuple4(final T t1,
+				  final U t2,
+				  final V t3,
+				  final W t4){
+		this.t1 = t1;
+		this.t2 = t2;
+		this.t3 = t3;
+		this.t4 = t4;
+	}
+
+	public T t1(){
+		return this.t1;
+	}
+
+	public U t2(){
+		return this.t2;
+	}
+
+	public V t3(){
+		return this.t3;
+	}
+
+	public W t4(){
+		return this.t4;
+	}
+
+}
